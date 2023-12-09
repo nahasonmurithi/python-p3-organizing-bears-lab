a04b30439dc1ddef464a2c47f6456a501b290ac9
Nahason <nahason.murithi@student.moringaschool.com>
2023-12-09 14:12:03 +0300

    Complete

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO 
+bears (id, name, age, sex, color, temperament, alive) VALUES 
+(1, "Mr. Chocolate", 5, "M", "Brown", "cool", True),
+(2, "Rowdy", 9, "M", "Green", "Angry", True),
+(3, "Tabitha", 20, "F", "Yellow", "Happy", True),
+(4, "Sergeant Brown", 16, "M", "Black", "Calm", False),
+(5, "Melissa", 19, "F", "Red", "tough", False),
+(6, "Grinch", 8, "M", "Pink", "sad", True),
+(7, "Wendy", 6, "F", "White", "Sassy", True),
+(8, NULL, NULL, "M", "Blue", NULL, True)
